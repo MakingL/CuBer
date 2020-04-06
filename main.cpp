47f3e14db9984e220491fe80490f6f1a83a52167
MLee <LJ1748503358@163.com>
2020-04-06 21:22:22 +0800

    Add: server main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,35 @@
-#include <iostream>
+#include "http/HttpServer.h"
+#include "http/HttpRequest.h"
+#include "http/HttpResponse.h"
+#include "net/EventLoop.h"
+#include "base/TimeZone.h"
+#include "base/Logging.h"
+#include "config/ServerConfig.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+#include <iostream>
+#include <map>
+
+using namespace cuber;
+using namespace cuber::net;
+using namespace cuber::config;
+
+int main(int argc, char *argv[]) {
+    cuber::TimeZone beijing(8 * 3600, "CST");
+    cuber::Logger::setTimeZone(beijing);
+
+    int numThreads = 0;
+    if (argc > 1) {
+        Logger::setLogLevel(Logger::WARN);
+        numThreads = atoi(argv[1]);
+    }
+
+    ServerConfig serverConf("conf/cuber.yaml");
+    serverConf.loadConfig();
+    auto server_info = serverConf.serverConf(8080);
+
+    EventLoop loop;
+    HttpServer server(&loop, InetAddress(server_info.listenPort), "CuBer", &serverConf);
+    server.setThreadNum(numThreads);
+    server.start();
+    loop.loop();
 }
